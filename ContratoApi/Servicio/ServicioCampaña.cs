2253a66d57af743cb1d2e2bb6476a19c2bc58da5
Matias Alarcon <matiasalarcon55@hotmail.com>
2022-11-13 18:22:37 -0300

    Vinculacion de nuevo servicio con el servicio actual

--- ContratoApi/Servicio/ServicioCampaña.cs
+++ ContratoApi/Servicio/ServicioCampaña.cs
@@ -1,50 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Donaciones.Contracts.DonacionesContrato;
+using Donaciones.Contracts.DonacionesContrato.ContractDefinition;
 using Microsoft.Extensions.Options;
 using Nethereum.Contracts;
 using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
-using Servicio.Contrato.Campania_v1.Campania;
-using Servicio.Contrato.Campania_v1.Campania.ContractDefinition;
 using Servicio.Models;
 
 namespace Servicio
 {
     public class ServicioCampaña : IServicioCampaña
     {
-        private CampaniaService _contratoCampaña;
+        private DonacionesContratoService _contrato;
         private readonly ContractData _config;
 
         public ServicioCampaña(IOptions<ContractData> options)
         {
-            _contratoCampaña = InstanciarContrato();
+            _contrato = InstanciarContrato();
             _config = options.Value;
         }
 
-        private CampaniaService InstanciarContrato()
+        private DonacionesContratoService InstanciarContrato()
         {
             var account = new Account(_config.WalletKey);
             var web3 = new Web3(account, _config.GoerliUrl);
          //   var web3 = new Web3(account, _config.PolygonUrl);
             web3.TransactionManager.UseLegacyAsDefault = true;
-            return new CampaniaService(web3, _config.GoerliContractHash);
+            return new DonacionesContratoService(web3, _config.GoerliContractHash);
           //  return new CampaniaService(web3, _config.PolygonContractHash);
         }
         
         public async Task<List<DonacionResponse>> ConsultarTodasLasDonaciones()
         {
-            return _contratoCampaña.ConsultarTodasLasDonacionesQueryAsync().Result.ReturnValue1;
+            return _contrato.ConsultarTodasLasDonacionesQueryAsync().Result.ReturnValue1;
         }
 
         public async Task<DonacionResponse> ConsultarDonacionPorId(int id)
         {
-            return _contratoCampaña.ConsultarDonacionesPorIdQueryAsync(id).Result.ReturnValue1;
+            return _contrato.ConsultarDonacionesPorIdQueryAsync(id).Result.ReturnValue1;
         }
 
         public async Task<object> CrearDonacion(DonacionRequest request)
         {
-            var response = await _contratoCampaña.CrearDonacionRequestAndWaitForReceiptAsync(new CrearDonacionFunction()
+            var response = await _contrato.CrearDonacionRequestAndWaitForReceiptAsync(new CrearDonacionFunction()
             {
                 Request = request
             });
@@ -55,7 +55,7 @@ namespace Servicio
 
         public async Task<List<DonacionResponse>> ConsultarDonacionesPorOrganizacion(int idOrganizacion)
         {
-            return _contratoCampaña.ConsultarDonacionesPorOrganizacionQueryAsync(idOrganizacion).Result.ReturnValue1;
+            return _contrato.ConsultarDonacionesPorOrganizacionQueryAsync(idOrganizacion).Result.ReturnValue1;
         }
     }
 }
